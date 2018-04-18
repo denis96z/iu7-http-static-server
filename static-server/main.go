535957ce27096e0715194672a8a233df11dedbff
denis96z <zinoviev.denis@yandex.ru>
2018-04-17 23:54:21 -0700

    issue #9. implemented config reader.

--- static-server/main.go
+++ static-server/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	var config ServerConfig
+	if config.FromFile("./config.json") != nil {
+		fmt.Println("failed to parse config file...")
+		return
+	}
 	if server := NewHttpServer(); server != nil {
 		if server.Start() != nil {
 			fmt.Println("failed to start server...")
